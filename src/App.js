79ee2edba6c05ff532f55ae7e52fd0843e4d7c96
sathishlakshs <sathishkumar3110@gmail.com>
2018-07-16 11:47:56 +0530

    router practice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import Helloworld from './components/Helloworld';
 import store from './store/store';
+import {BrowserRouter as Router, Link, NavLink, Redirect, Prompt} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 import './App.scss';
+
+const User=(props)=>{
+    return (<h1>hai {props.username}</h1>);
+}
+
 export default class App extends React.Component{
+    state={
+        loggedIn:false
+    };
+
+    loginHandle=()=>{
+        this.setState(prevState =>({
+            loggedIn:!prevState.loggedIn
+        }))
+    }
+
     render(){
         
         store.subscribe(() =>{
@@ -12,9 +29,33 @@ export default class App extends React.Component{
         store.dispatch({type:'BackEnd', payload:"Salman"})
 
         return (
-            <div className='App'>
-            <Helloworld />
-            </div>
+            <Router>
+               <div>
+                   <ul>
+                        <li><NavLink to="/" exact activeStyle={{color:'green'}}>home</NavLink> </li>
+                        <li><NavLink to="/ui" exact activeStyle={{color:'green'}}>frontend developer</NavLink> </li>
+                        <li><NavLink to="/db" exact activeStyle={{color:'green'}}>backend developer</NavLink> </li>
+                        <li><NavLink to="/user/marees" exact activeStyle={{color:'green'}}>user</NavLink> </li>
+                   </ul>
+                <Prompt
+                    when={!this.state.loggedIn} message={(location)=>{
+                        return location.pathname.startsWith('/user')? 'Are u sure?':true
+                    }} />
+                <input type='button' value={this.state.loggedIn?'log Out':'log In'} onClick={this.loginHandle}/>
+
+                <Route path='/' exact strict render = {
+                    ()=>{return(<h1>Welcome sathishlakshs</h1>)}
+                }/>
+                 <Route path='/ui' exact strict render = {
+                    ()=>{return(<h1>Hai Hari</h1>)}
+                }/>
+                 <Route path='/db' exact strict render = {
+                    ()=>{return(<h1>Hai salman</h1>)}
+                }/>
+                <Route path='/user/:username' exact strict render={
+                    ({match})=>(this.state.loggedIn? (<User username={match.params.username} />) : (<Redirect to="/" />))}/>
+                </div>
+            </Router>
         );
     }
 }
